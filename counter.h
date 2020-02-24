0cb7f6e6b6ea76085a4faa70144af83f94daf06b
ikachko <ilyakachko@gmail.com>
2020-02-24 20:52:43 +0200

    Added todo comment

--- counter.h
+++ counter.h
@@ -8,6 +8,7 @@ using Dict = std::map<std::string, size_t>;
 
 class WordCounter {
 public:
+	// TODO: implement WordCounter::process
     Dict process(const std::string& file);
 private:
     Dict wc;
