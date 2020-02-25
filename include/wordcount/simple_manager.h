56838523c5951203e152a491091ab21082a1d724
lekhovitsky <lekhovitsky@gmail.com>
2020-02-25 16:32:18 +0200

    Move save() to BaseManager

--- include/wordcount/simple_manager.h
+++ include/wordcount/simple_manager.h
@@ -5,20 +5,20 @@
 #include <vector>
 #include "base_manager.h"
 
-class SimpleManager : BaseManager {
+class SimpleManager : public BaseManager {
 public:
     SimpleManager(
             std::string infile,
             std::string out_by_a,
             std::string out_by_n,
             size_t n_threads)
-            : infile {std::move(infile) }
+            : BaseManager{ out_by_a, out_by_n }
+            , infile {std::move(infile) }
             , out_by_a { std::move(out_by_a) }
             , out_by_n { std::move(out_by_n) }
             , n_threads { n_threads } {}
     void read();
     void count();
-    void save();
 private:
     std::string infile;
     std::string out_by_a;
--- src/simple_manager.cpp
+++ src/simple_manager.cpp
@@ -35,10 +35,3 @@ void SimpleManager::count() {
         wc = std::accumulate(dicts.begin(), dicts.end(), Dict{}, merge_dicts);
     }
 }
-
-void SimpleManager::save() {
-    auto wc_by_a = sort_by_a(wc);
-    save_dict(out_by_a, wc_by_a);
-    auto wc_by_n = sort_by_n(wc);
-    save_dict(out_by_n, wc_by_n);
-}
